7022b610216836e56ff96960aa0063f83ca96087
caopengxu <762095508@qq.com>
2018-08-14 14:30:00 +0800

    初始化

--- Keyboard/ViewController.swift
+++ Keyboard/ViewController.swift
@@ -9,54 +9,112 @@
 import UIKit
 import SceneKit
 import ARKit
+//import SpriteKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var scene: SCNScene!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        scene = SCNScene(named: "art.scnassets/GameScene.scn")!
         
-        // Set the scene to the view
         sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        let configuration = ARImageTrackingConfiguration()
+        guard let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "Images", bundle: Bundle.main) else {
+            return
+        }
+        configuration.trackingImages = trackedImages
+        configuration.maximumNumberOfTrackedImages = 2
+//        configuration.worldAlignment = .gravityAndHeading
+        configuration.worldAlignment = .camera
+        
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+    
+    
+    // MARK: === RSCNViewDelegate
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode?
+    {
         let node = SCNNode()
-     
+        
+        if let imageAnchor = anchor as? ARImageAnchor
+        {
+            print(imageAnchor.transform)
+            
+            if (imageAnchor.name == "mouse")
+            {
+                let nikename = SCNText(string: "这是一个鼠标", extrusionDepth: 0.01)
+                nikename.firstMaterial?.diffuse.contents = UIColor.red
+                nikename.font = UIFont(name: "BradleyHandITCTT-Bold", size: 0.2)
+                
+//                let a = imageAnchor.transform.columns
+//                let b = imageAnchor.transform.debugDescription
+//                let c = imageAnchor.transform.determinant
+//                let d = imageAnchor.transform.inverse
+//                let e = imageAnchor.transform.transpose
+                
+                let nikenameNode = SCNNode(geometry: nikename)
+                let mat = SCNMatrix4(imageAnchor.transform)
+                
+                
+                let dir = SCNVector3(1 * mat.m31, 1 * mat.m32, 1 * mat.m33)
+                let pos = SCNVector3(mat.m41, mat.m42, mat.m43)
+//                nikenameNode.transform = SCNMatrix4(imageAnchor.transform)
+//                nikenameNode.transform = SCNMatrix4MakeRotation(-.pi / 2, 1, 0, 0)
+                print(dir)
+                
+                nikenameNode.rotation = SCNVector4(1, 0, 0, -Float.pi / 2)
+//                nikenameNode.position = SCNVector3(0, 0, 0)
+                nikenameNode.position = pos
+               
+                scene.rootNode.addChildNode(nikenameNode)
+                
+//                node.addChildNode(nikenameNode)
+            }
+            else
+            {
+                let password = SCNText(string: "这是一片纸巾这是一片纸巾这是一片纸巾这是一片纸巾这是一片纸巾", extrusionDepth: 0.1)
+                password.firstMaterial?.diffuse.contents = UIColor.red
+                password.font = UIFont(name: "BradleyHandITCTT-Bold", size: 0.5)
+                let passwordNode = SCNNode(geometry: password)
+                
+                passwordNode.transform = SCNMatrix4MakeRotation(-.pi / 2, 1, 0, 0)
+                
+                node.addChildNode(passwordNode)
+            }
+        }
+        
         return node
     }
-*/
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval)
+    {
+//        print(time)
+    }
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor)
+    {
+        print(node)
+        print(anchor)
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
