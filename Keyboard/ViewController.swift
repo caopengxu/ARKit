b82d49963a58a0fcb87b9426b8c502589a6979bf
caopengxu <762095508@qq.com>
2018-08-17 18:27:31 +0800

    基本搞定

--- Keyboard/ViewController.swift
+++ Keyboard/ViewController.swift
@@ -9,37 +9,40 @@
 import UIKit
 import SceneKit
 import ARKit
-//import SpriteKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     var scene: SCNScene!
+    var once = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         sceneView.delegate = self
-        
         sceneView.showsStatistics = true
-        
         scene = SCNScene(named: "art.scnassets/GameScene.scn")!
-        
         sceneView.scene = scene
     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        let configuration = ARImageTrackingConfiguration()
+        let configuration = ARWorldTrackingConfiguration()
+//        let configuration = ARImageTrackingConfiguration()
         guard let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "Images", bundle: Bundle.main) else {
             return
         }
-        configuration.trackingImages = trackedImages
+//        configuration.trackingImages = trackedImages
+//        configuration.maximumNumberOfTrackedImages = 2
+        
+        configuration.detectionImages = trackedImages
         configuration.maximumNumberOfTrackedImages = 2
-//        configuration.worldAlignment = .gravityAndHeading
-        configuration.worldAlignment = .camera
+        
+        configuration.worldAlignment = .gravityAndHeading
+//        configuration.worldAlignment = .camera
         
         sceneView.session.run(configuration)
     }
@@ -53,81 +56,84 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     
     // MARK: === RSCNViewDelegate
+    func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval)
+    {
+    }
+    func renderer(_ renderer: SCNSceneRenderer, willUpdate node: SCNNode, for anchor: ARAnchor)
+    {
+    }
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor)
+    {
+    }
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode?
     {
         let node = SCNNode()
         
         if let imageAnchor = anchor as? ARImageAnchor
         {
-            print(imageAnchor.transform)
+            let w = Float(imageAnchor.referenceImage.physicalSize.width)
+            let h = Float(imageAnchor.referenceImage.physicalSize.height)
             
-            if (imageAnchor.name == "mouse")
-            {
-                let nikename = SCNText(string: "这是一个鼠标", extrusionDepth: 0.01)
-                nikename.firstMaterial?.diffuse.contents = UIColor.red
-                nikename.font = UIFont(name: "BradleyHandITCTT-Bold", size: 0.2)
-                
-//                let a = imageAnchor.transform.columns
-//                let b = imageAnchor.transform.debugDescription
-//                let c = imageAnchor.transform.determinant
-//                let d = imageAnchor.transform.inverse
-//                let e = imageAnchor.transform.transpose
-                
-                let nikenameNode = SCNNode(geometry: nikename)
-                let mat = SCNMatrix4(imageAnchor.transform)
-                
-                
-                let dir = SCNVector3(1 * mat.m31, 1 * mat.m32, 1 * mat.m33)
-                let pos = SCNVector3(mat.m41, mat.m42, mat.m43)
-//                nikenameNode.transform = SCNMatrix4(imageAnchor.transform)
-//                nikenameNode.transform = SCNMatrix4MakeRotation(-.pi / 2, 1, 0, 0)
-                print(dir)
-                
-                nikenameNode.rotation = SCNVector4(1, 0, 0, -Float.pi / 2)
-//                nikenameNode.position = SCNVector3(0, 0, 0)
-                nikenameNode.position = pos
-               
-                scene.rootNode.addChildNode(nikenameNode)
-                
-//                node.addChildNode(nikenameNode)
-            }
-            else
-            {
-                let password = SCNText(string: "这是一片纸巾这是一片纸巾这是一片纸巾这是一片纸巾这是一片纸巾", extrusionDepth: 0.1)
-                password.firstMaterial?.diffuse.contents = UIColor.red
-                password.font = UIFont(name: "BradleyHandITCTT-Bold", size: 0.5)
-                let passwordNode = SCNNode(geometry: password)
-                
-                passwordNode.transform = SCNMatrix4MakeRotation(-.pi / 2, 1, 0, 0)
-                
-                node.addChildNode(passwordNode)
-            }
+            
+            let mat = SCNMatrix4(anchor.transform)
+            let dir = SCNVector3(-1 * mat.m31, -1 * mat.m32, -1 * mat.m33)
+            let pos = SCNVector3(mat.m41, mat.m42, mat.m43)
+            
+//            var offset = matrix_identity_float4x4
+//            offset.columns.3.z = 0.2
+//            let transform = simd_mul(anchor.transform, offset)
+            
+            
+            let text1 = SCNText(string: "这是个鼠标", extrusionDepth: 0.01)
+            text1.firstMaterial?.diffuse.contents = UIImage(named: "1.jpg")
+            text1.font = UIFont(name: "BradleyHandITCTT-Bold", size: 0.01)
+            let textNode1 = SCNNode(geometry: text1)
+            textNode1.transform = mat
+//            textNode1.position = SCNVector3(0, 0, 0)
+            
+            let text2 = SCNText(string: "这个是个logo", extrusionDepth: 0.1)
+            text2.firstMaterial?.diffuse.contents = UIImage(named: "1.jpg")
+            text2.font = UIFont(name: "BradleyHandITCTT-Bold", size: 0.1)
+            let textNode2 = SCNNode(geometry: text2)
+            textNode2.transform = mat
+//            textNode2.position = SCNVector3(-w / 2, -h / 2, pos.z)
+            
+            let text3 = SCNText(string: "看右边", extrusionDepth: 0.1)
+            text3.firstMaterial?.diffuse.contents = UIImage(named: "1.jpg")
+            text3.font = UIFont(name: "BradleyHandITCTT-Bold", size: 0.1)
+            let textNode3 = SCNNode(geometry: text3)
+            textNode3.transform = mat
+//            textNode3.position = SCNVector3(-w, -h, pos.z)
+            
+            
+            let moon = SCNSphere(radius: 0.005)
+            moon.firstMaterial?.diffuse.contents = UIImage(named: "2.jpg")
+            let moonNode = SCNNode(geometry: moon)
+            moonNode.position = SCNVector3(0, 0, 0)
+            
+            let earth = SCNSphere(radius: 0.01)
+            earth.firstMaterial?.diffuse.contents = UIImage(named: "3.jpg")
+            let earthNode = SCNNode(geometry: earth)
+            earthNode.position = SCNVector3(w / 2, 0, 0)
+            
+            let sphere = SCNSphere(radius: 0.03)
+            sphere.firstMaterial?.diffuse.contents = UIImage(named: "1.jpg")
+            let sphereNode = SCNNode(geometry: sphere)
+            sphereNode.position = SCNVector3(w, 0, 0)
+            
+//            node.addChildNode(textNode1)
+//            node.addChildNode(textNode2)
+//            node.addChildNode(textNode3)
+            node.addChildNode(moonNode)
+            node.addChildNode(earthNode)
+            node.addChildNode(sphereNode)
         }
-        
         return node
-    }
-    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval)
-    {
-//        print(time)
     }
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor)
     {
-        print(node)
-        print(anchor)
     }
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor)
+    {
     }
 }
